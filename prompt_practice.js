9d468da1b27b811f7f3a30460a12e4da698b5554
jake32321 <jake32321@yahoo.com>
2015-11-23 23:18:13 -0600

    making the first commit

---
+++ prompt_practice.js
@@ -0,0 +1,10 @@
+//Asks the user for their first name.
+alert('What is your first name?');
+var firstName = prompt();
+
+//Asks the user for their last name.
+alert('What is your last name?');
+var lastName = prompt();
+
+//Sends alert to the user with their first and last name as one string
+alert(firstName+lastName);
