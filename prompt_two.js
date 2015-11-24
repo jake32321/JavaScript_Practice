6293fc73c4909b757bd57001a35be7e29ffa00e6
jake32321 <jake32321@yahoo.com>
2015-11-23 23:22:43 -0600

    added another prompt practice

---
+++ prompt_two.js
@@ -0,0 +1,14 @@
+//Asks the user for their age
+alert('What is your age?');
+var age = prompt();
+
+//Asks the user for their first name
+alert('What is your first name?');
+var firstName = prompt();
+
+//Asks the user for their last name
+alert('What is your last name?');
+var lastName = prompt();
+
+//Sends message to the user via alert
+alert('Hello, '+firstName+' '+lastName+'! You are '+age+' years old today.').
